17cd031a1dad6e709b2e50986b67ec2088556506
CatUnderGlue <warmonds1996@gmail.com>
2023-04-05 19:38:01 +0500

    Initial commit

---
+++ src/main/java/ru/codehunters/zaepestelegrambot/ZaepesTelegramBotApplication.java
@@ -0,0 +1,13 @@
+package ru.codehunters.zaepestelegrambot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZaepesTelegramBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZaepesTelegramBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/codehunters/zaepestelegrambot/ZaepesTelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.codehunters.zaepestelegrambot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZaepesTelegramBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
