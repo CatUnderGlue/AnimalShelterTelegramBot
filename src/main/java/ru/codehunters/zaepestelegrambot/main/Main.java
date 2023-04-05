6163b1bff8ea741fcd3375d0d06e217a3cdb885d
Mexx <mexx0174@gmail.com>
2023-04-05 19:51:36 +0500

    1 com

--- src/main/java/ru/codehunters/zaepestelegrambot/main/Main.java
+++ src/main/java/ru/codehunters/zaepestelegrambot/main/Main.java
@@ -2,6 +2,6 @@ package ru.codehunters.zaepestelegrambot.main;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hi");
+        System.out.println("hi") ;
     }
 }
