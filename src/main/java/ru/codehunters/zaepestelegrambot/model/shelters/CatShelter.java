a132abef45c69095813c696d7b2cf1a8c63d83b0
Ilyas <xxxilyas92xxx@gmail.com>
2023-04-08 00:58:43 +0500

    Добавил поля моделей

--- src/main/java/ru/codehunters/zaepestelegrambot/model/shelters/CatShelter.java
+++ src/main/java/ru/codehunters/zaepestelegrambot/model/shelters/CatShelter.java
@@ -1,4 +1,23 @@
 package ru.codehunters.zaepestelegrambot.model.shelters;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import ru.codehunters.zaepestelegrambot.model.animals.Cat;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class CatShelter{
+   private String name; // Название приюта
+   private String location; // Адрес и схема проезда
+   private String timetable; // Расписание
+   private String aboutMe; // О приюте
+   private List<Cat> list; // Список животных в приюте
+   private String security; // Способ связи с охраной
+   private String safetyAdvice; // Рекомендации о технике безопасности на территории приюта
+
+
 }
--- src/main/java/ru/codehunters/zaepestelegrambot/model/shelters/DogShelter.java
+++ src/main/java/ru/codehunters/zaepestelegrambot/model/shelters/DogShelter.java
@@ -1,4 +1,21 @@
 package ru.codehunters.zaepestelegrambot.model.shelters;
 
-public class DogShelter{
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import ru.codehunters.zaepestelegrambot.model.animals.Dog;
+
+import java.util.List;
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public class DogShelter{
+        private String name; // Название приюта
+        private String location; // Адрес и схема проезда
+        private String timetable; // Расписание
+        private String aboutMe; // О приюте
+        private List<Dog> list; // Список животных в приюте
+        private String security; // Способ связи с охраной
+        private String safetyAdvice; // Рекомендации о технике безопасности на территории приюта
 }
