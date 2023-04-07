705faa1d9aa14c349795470decc84f01f783f693
Mexx <mexx0174@gmail.com>
2023-04-07 17:17:55 +0500

    Добавил структуру проекта

---
+++ src/main/java/ru/codehunters/zaepestelegrambot/model/owners/CatOwner.java
@@ -0,0 +1,7 @@
+package ru.codehunters.zaepestelegrambot.model.owners;
+
+
+import ru.codehunters.zaepestelegrambot.model.User;
+
+public class CatOwner extends User {
+}
---
+++ src/main/java/ru/codehunters/zaepestelegrambot/model/owners/DogOwner.java
@@ -0,0 +1,6 @@
+package ru.codehunters.zaepestelegrambot.model.owners;
+
+import ru.codehunters.zaepestelegrambot.model.User;
+
+public class DogOwner extends User {
+}
