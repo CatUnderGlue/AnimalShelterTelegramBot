5e39b9cf2992734e41ad6379aa5e0376f615ab97
profmi2022 <profmi2022@gmail.com>
2023-04-08 19:36:09 +0600

    Созданы сущности User, CatOwner, DogOwner с репозиториями

--- src/main/java/ru/codehunters/zaepestelegrambot/model/User.java
+++ src/main/java/ru/codehunters/zaepestelegrambot/model/User.java
@@ -1,4 +1,27 @@
 package ru.codehunters.zaepestelegrambot.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
 public class User {
+
+//    private Long id;
+
+    @Id
+    @Column
+    private String telegramId;
+
+    @Column
+    private String name;
+
+    @Column
+    private String lastName;
+
 }
--- src/main/java/ru/codehunters/zaepestelegrambot/model/owners/CatOwner.java
+++ src/main/java/ru/codehunters/zaepestelegrambot/model/owners/CatOwner.java
@@ -1,4 +1,30 @@
 package ru.codehunters.zaepestelegrambot.model.owners;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import ru.codehunters.zaepestelegrambot.model.User;
+import ru.codehunters.zaepestelegrambot.model.animals.Cat;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "catowner")
 public class CatOwner{
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @OneToOne(cascade = CascadeType.ALL, mappedBy = "telegramId")
+//    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private User user;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "ownerId")
+//    @JoinColumn(name = "id", referencedColumnName = "catOwner_id")
+    private List<Cat> catList;
 }
