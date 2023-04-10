3dfb165d2f98b32ae6141567931a4767e46c2e7a
CatUnderGlue <warmonds1996@gmail.com>
2023-04-10 17:34:39 +0500

    Удалил неиспользуемые иморты

--- src/main/java/ru/codehunters/zaepestelegrambot/controller/UserController.java
+++ src/main/java/ru/codehunters/zaepestelegrambot/controller/UserController.java
@@ -2,8 +2,6 @@ package ru.codehunters.zaepestelegrambot.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
